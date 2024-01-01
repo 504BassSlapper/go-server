23af3a4625b35e8a3dc6371ca7cd4a82d5d092fb
504BassSlapper <504BassSlapper@gmail.com>
2024-01-01 13:40:02 +0100

    add example html page

--- handler/template_handler.go
+++ handler/template_handler.go
@@ -1,17 +1,48 @@
 package handler
 
 import (
+	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 )
 
 func TemplateHandler(w http.ResponseWriter, r *http.Request) {
-	// to implement
-	layoutPath := filepath.Join("static", "tempaltes", "layout")
-	fp := filepath.Join("templates", filepath.Clean(r.URL.Path))
 
-	temp, _ := template.ParseFiles(layoutPath, fp)
-	temp.ExecuteTemplate(w, "layout", nil)
+	layoutPath := filepath.Join("static", "templates", "layout.html")
+	fp := filepath.Join("static", "templates", filepath.Clean(r.URL.Path))
+
+	// Return a 404 if the template doesn't exist
+	pwd, err := os.Getwd()
+	fmt.Println("pwd: %v", pwd)
+	info, err := os.Stat(fp)
+	if err != nil {
+		if os.IsNotExist(err) {
+			http.NotFound(w, r)
+			return
+		}
+	}
+
+	// Return a 404 if the request is for a directory
+	if info.IsDir() {
+		http.NotFound(w, r)
+		return
+	}
+
+	tmpl, err := template.ParseFiles(layoutPath, fp)
+	if err != nil {
+		// Log the detailed error
+		log.Print(err.Error())
+		// Return a generic "Internal Server Error" message
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+	err = tmpl.ExecuteTemplate(w, "layout.html", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, http.StatusText(500), 500)
+	}
 
 }
--- main.go
+++ main.go
@@ -10,10 +10,10 @@ import (
 
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fileServer)
 	http.HandleFunc("/home", handler.HomeHandler)
 	http.HandleFunc("/form", handler.FormHandler)
 	http.Handle("/static/", http.StripPrefix("/static/", fileServer))
+	http.HandleFunc("/", handler.TemplateHandler)
 	fmt.Println("Server starts on port 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatalf("Server stopped: %v", err)
